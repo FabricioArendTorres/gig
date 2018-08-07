6f3f7f7f8ee4b65cb0c31dde67763fe8b08c59a8
Fabricio Arend Torres <arefab00@dmi-abidjan.dmi.unibas.ch>
2018-08-07 22:23:23 +0200

    added name, version and author

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("gig.pyx", language="c++"))
+setup(name='gig', version='0.1', author='Fabricio Arend Torres', ext_modules=cythonize("gig.pyx"))
